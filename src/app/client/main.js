4c527447c15a4d661ea8cc74d90c388f5cf8a59c
sbratulescu <sbratulescu@w-systems.com>
2017-06-22 00:45:05 +0300

    Removed onsen UI kit and added bootstrap instead

--- src/app/client/main.js
+++ src/app/client/main.js
@@ -7,9 +7,15 @@ import {
 
 import './main.html';
 
+import { 
+    Blaze 
+} from 'meteor/blaze';
+
 if (Meteor.isClient) {
     Session.keys = {};
-    
+
+    Meteor.methods({
+    });
     Template.processPhoto.events({
         'click .take-photo': function() {
             var cameraOpts = {
@@ -19,11 +25,25 @@ if (Meteor.isClient) {
             };
 
             var callback = function(error, data) {
-                if (error)
-                    Session.set('error_taking_photo', error);
+                if (error) {
+                    alertMessage = "Error selecting photo!";
+                    alertClass = 'alert-danger';
+                }
 
-                if (data)
-                    Session.set('test', data);
+                if (data) {
+                    alertMessage = "Photo has been successfully selected!";
+                    alertClass = 'alert-success';
+
+                    Session.set('photoSelected', data);
+                }
+
+                $('#step1').find('.alert').addClass(alertClass);
+                $('#step1').find('.alert').css('display', 'block');
+
+                $('#step1').find('.next-step').removeClass('disabled');
+                $('#step1').find('.next-step').css('pointer-events', 'all');
+
+                Session.set('alertMessage', alertMessage);
             };
 
             MeteorCamera.getPicture(cameraOpts, callback);
@@ -37,41 +57,110 @@ if (Meteor.isClient) {
             };
 
             var callback = function(error, data) {
-                if (error)
-                    Session.set('error_choosing_photo', error);
+                if (error) {
+                    alertMessage = "Error selecting photo!";
+                    alertClass = 'alert-danger';
+                }
 
-                if (data)
-                    Session.set('test', data);
+                if (data) {
+                    alertMessage = "Photo has been successfully selected!";
+                    alertClass = 'alert-success';
+
+                    $('#step1').find('.next-step').removeClass('disabled');
+                    $('#step1').find('.next-step').css('pointer-events', 'all');
+
+                    Session.set('photoSelected', data);
+                }
+
+                $('#step1').find('.alert').addClass(alertClass);
+                $('#step1').find('.alert').css('display', 'block');
+
+                Session.set('alertMessage', alertMessage);
             };
 
             MeteorCamera.getPicture(cameraOpts, callback);
         },
         'click .process-photo': function() {
-        	var photo = Session.get('test') || '';
+            waitingDialog.show('Photo is being processed...');
+
+        	var photo = Session.get('photoSelected') || '';
 
             if (photo === '') {
-                alert('No photo selected!');
+                photoProcessed = "No photo selected.";
+                alertClass = 'alert-danger';
+
+                $('#step2').find('.alert').addClass(alertClass);
+                $('#step2').find('.alert').css('display', 'block');
+
+                Session.set('photoProcessed', photoProcessed);
+
+                waitingDialog.hide();
 
                 return;
             }
 
             Meteor.call('processFile', photo, function (error, data) {
-                if (data.status === 'success') {
-                    Session.set('file_processed', true);
-                } else {
-                    Session.set('file_processed', false);
+                waitingDialog.hide();
+
+                if (error) {
+                    photoProcessed = "Error processing photo!";
+                    alertClass = 'alert-danger';
                 }
-                alert(data.data);
+
+                if (data) {
+                    photoProcessed = "Photo has been successfully processed";
+                    alertClass = 'alert-success';
+
+                    $('#step2').find('.next-step').removeClass('disabled');
+                    $('#step2').find('.next-step').css('pointer-events', 'all');
+                }
+
+                $('#step2').find('.alert').addClass(alertClass);
+                $('#step2').find('.alert').css('display', 'block');
+
+                Session.set('photoProcessed', photoProcessed);
 			});
         },
         'click .view-content': function() {
             Meteor.call('getContent', function (error, data) {
-                alert(data.data);
+                if (error) {
+                    photoContent = "Error extracting photo content!";
+                    alertClass = 'alert-danger';
+                }
+
+                if (data) {
+                    photoContent = data.data
+                    alertClass = 'alert-success';
+                }
+
+                Session.set('photoContent', photoContent);
             })
         },
         'click .generate-pdf': function() {
+            var pdfName = Session.get('pdfName') || '';
+
+            waitingDialog.show('PDF is being generated...');
+
             Meteor.call('generatePDF', function(error, data) {
-                alert(data.data);
+                waitingDialog.hide();
+                
+                if (error) {
+                    pdfGenerated = "Error generating PDF!";
+                    alertClass = 'alert-danger';
+                }
+
+                if (data) {
+                    pdfGenerated = "PDF has been successfully generated!";
+                    alertClass = 'alert-success';
+
+                    $('#step3').find('.next-step').removeClass('disabled');
+                    $('#step3').find('.next-step').css('pointer-events', 'all');
+                }
+
+                $('#step3').find('.alert').addClass(alertClass);
+                $('#step3').find('.alert').css('display', 'block');
+
+                Session.set('pdfGenerated', pdfGenerated);
             })
         },
         'click .send-to-sugar': function() {
@@ -81,14 +170,23 @@ if (Meteor.isClient) {
         },
     });
     Template.processPhoto.helpers({
-        'photo': function() {
-            return Session.get('test');
+        'alertMessage': function() {
+            return Session.get('alertMessage');
         },
-        'error': function() {
-            return Session.get('error');
+        'photoSelected': function() {
+            return Session.get('photoSelected');
         },
-        'file_processed': function() {
-            return Session.get('file_processed');
+        'photoProcessed': function() {
+            return Session.get('photoProcessed');
+        },
+        'photoContent': function() {
+            return Session.get('photoContent');
+        },
+        'pdfName': function() {
+            return Session.get('pdfName');
+        },
+        'pdfGenerated': function() {
+            return Session.get('pdfGenerated');
         },
     });
 }
